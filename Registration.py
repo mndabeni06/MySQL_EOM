f411950aac29d495add5ed5a6943bd64e25b2282
Masimthembe <mndabeni6@gmail.com>
2021-07-07 16:36:53 +0200

    updated changes

---
+++ Registration.py
@@ -0,0 +1,129 @@
+from tkinter import messagebox
+from tkinter import*
+import mysql.connector
+reg = Tk()
+
+
+class Registration:
+
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Lifechoices Online')
+        self.master.geometry('500x360')
+        self.master.configure(bg='#F49F1C')
+
+
+        self.name = Label(master, text="Please Enter Your Fullname")
+        self.name.place(x=10, y=5)
+        self.name_entry = Entry(master, borderwidth=5)
+        self.name_entry.place(x=250, y=5)
+
+        self.surname = Label(master, text="Please Enter Your Lastname")
+        self.surname.place(x=10, y=50)
+        self.surname_entry = Entry(master, borderwidth=5)
+        self.surname_entry.place(x=250, y=50)
+
+        self.phone_number = Label(master, text="Please Enter Your Phone_number")
+        self.phone_number.place(x=10, y=100)
+        self.phone_number_entry = Entry(master, borderwidth=5)
+        self.phone_number_entry.place(x=250, y=100)
+
+
+        self.next_of_kin_name = Label(master, text="Next of Kin Name")
+        self.next_of_kin_name.place(x=10, y=150)
+        self.next_of_kin_name_entry = Entry(master, borderwidth=5)
+        self.next_of_kin_name_entry.place(x=250, y=150)
+
+        self.next_of_kin_mobile_number = Label(master, text="Next of Kin Mobile Number")
+        self.next_of_kin_mobile_number.place(x=10, y=200)
+        self.next_of_kin_mobile_number_entry = Entry(master, borderwidth=5)
+        self.next_of_kin_mobile_number_entry.place(x=250, y=200)
+
+
+        self.register = Button(master, text="Click Here For Registration", borderwidth=5, bg="orange", command=self.register)
+        self.register.place(x=5, y=300)
+
+        self.exit_btn = Button(master, text="Exit", borderwidth=5, bg="red", command=self.exit)
+        self.exit_btn.place(x=400, y=300)
+
+
+
+    def register(self):
+
+         global mydb
+
+         if self.name_entry.get() == "" and self.surname_entry.get() == "" and self.phone_number_entry.get() and self.next_of_kin_name_entry.get() == "" and self.next_of_kin_mobile_number_entry.get() == "":
+                messagebox.showerror("STATUS", "PLEASE ENTER VALID DETAILS")
+
+         else:
+            mydb = mysql.connector.connect(user='lifechoices', password='@Lifechoices1234', host='127.0.0.1',
+                                           database='LifechoicesOnline',
+                                           auth_plugin='mysql_native_password')
+            mycursor = mydb.cursor()
+            sql = "INSERT INTO Registration (name, surname, phone_number, next_of_kin_name, next_of_kin_mobile_numbe) VALUES (%s, %s, %s, %s, %s)"
+            val = (self.name_entry.get(), self.surname_entry.get(), self.phone_number_entry.get(), self.next_of_kin_name_entry.get(), self.next_of_kin_mobile_number_entry.get())
+            mycursor.execute(sql, val)
+            messagebox.showinfo("PERMISSION", "Successfully Registered")
+            self.name_entry.delete(0, END)
+            self.surname_entry.delete(0, END)
+            self.phone_number_entry.delete(0, END)
+            self.next_of_kin_name_entry.delete(0, END)
+            self.next_of_kin_mobile_number_entry.delete(0, END)
+            mydb.commit()
+
+
+
+    def exit(self):
+        self.messagebox = messagebox.askquestion('Exit Application', 'Are you sure you want to Logout?')
+        if self.messagebox == 'yes':
+            self.master.destroy()
+        else:
+            pass
+
+
+
+
+
+
+
+
+
+    #def admin_login(self):
+   # adminPassword = entUserPass.get()
+
+   # mydb = mysql.connector.connect(
+                                   # host="127.0.0.1",
+                                    #user="lifechoices",
+                                    #password="@Lifechoices1234",
+                                    #database="LifechoicesOnline")
+   # mycursor = mydb.cursor()
+   # sql = 'SELECT * FROM Registration =%s'
+    #mycursor.execute(sql, [adminPassword])
+   # res = mycursor.fetchall()
+    #if res:
+        #for i in res:
+            #logged()
+           # break
+    #else:
+       # failed()
+
+
+#def logged():
+    #messagebox.showinfo('Correct', 'you have successfully logged into admin')
+    #pass
+
+
+
+#def failed():
+    #messagebox.showinfo('Failed', 'Please enter correct admin password')
+   # entUserPass.focus()
+
+
+
+
+
+
+
+
+Z = Registration(reg)
+reg.mainloop()
--- Vistors_Login.py
+++ Vistors_Login.py
@@ -1,93 +1,74 @@
-from tkinter import *
 from tkinter import messagebox
-from datetime import datetime
+from tkinter import*
 import mysql.connector
-
-now = datetime.now()
-formatted = now.strftime('%Y-%m-%d %H:%M:%S')
-
-from PIL import Image, ImageTk
-
 root = Tk()
 
-background_img = Image.open("background.png")
-bg_img = ImageTk.PhotoImage(background_img)
-img = Label(root, image=bg_img)
-img.place(x=0, y=0)
 
+class Admin:
 
-class Database:
     def __init__(self, master):
-        self.root = root
-        self.root.title('')
-        self.root.geometry("500x360")
+        self.master = master
+        self.master.title('Lifechoices Online')
+        self.master.geometry('400x400')
+        self.master.configure(bg='#F49F1C')
 
-        self.username = Label(master, text="Please Enter Username", borderwidth=5)
-        self.username.place(x=5, y=5)
-        self.username_entry = Entry(master, borderwidth=5)
-        self.username_entry.place(x=200, y=5)
+        self.admin_header = Label(master,  bg='#F49F1C', fg='#0F52BA', text='Administrator Login Page')
+        self.admin_header.place(x=50, y=50, width=300)
+        self.admin_header.config(font=('courier', '14', 'bold'))
 
-        self.password = Label(master, text="Please Enter password", borderwidth=5)
-        self.password.place(x=5, y=50)
-        self.password_entry = Entry(master, borderwidth=5)
-        self.password_entry.place(x=200, y=50)
+        self.admin_username = Label(master,  bg='#F49F1C', fg='white', text='Please Enter Admin Username')
+        self.admin_username.place(x=150, y=100)
 
-        self.login_btn = Button(master, text="Login", borderwidth=5, command=self.login, bg="goldenrod")
-        self.login_btn.place(x=10, y=200)
+        self.admin_entry = Entry(master, width=45)
+        self.admin_entry.place(x=140, y=150, width=120)
 
-        self.register_user_btn = Button(master, text="Register New User", borderwidth=5, command=self.register,
-                                        bg="yellow")
-        self.register_user_btn.place(x=150, y=200)
+        self.admin_password = Label(master,  bg='#F49F1C', fg='#0F52BA', text='Please Enter Admin Password')
+        self.admin_password.place(x=150, y=150)
 
-        self.exit_btn = Button(master, text="Exit", borderwidth=5, bg="red", command=self.exit)
-        self.exit_btn.place(x=400, y=200)
+        self.password_entry = Entry(master, width=45)
+        self.password_entry
 
-    def login(self):
-
-        if self.username_entry.get() == "" and self.password_entry.get() == "":
-            messagebox.showerror("STATUS", "PLEASE ENTER VALID DETAILS")
-        else:
-            hospital = mysql.connector.connect(user='lifechoices', password='@Lifechoices1234', host='127.0.0.1',
-                                               database='lifechoicesonline',
-                                               auth_plugin='mysql_native_password')
-            mycursor = hospital.cursor()
-            xy = mycursor.execute('Select * from users')
-            for x in mycursor:
-                if x[1] == self.password_entry.get() and x[0] == self.username_entry.get():
-                    messagebox.showinfo("PERMISSION", "SUCCESSFUL! Enjoy Your Day")
-                    root.destroy()
-            if x[1] != self.password_entry.get() or x[0] != self.username_entry.get():
-                messagebox.showerror("STATUS", "ACCESS DENIED")
-                self.username_entry.delete(0, END)
-                self.password_entry.delete(0, END)
-
-    def register(self):
-
-        global reg
-        if self.username_entry.get() == "" and self.password_entry.get() == "":
-            messagebox.showerror("STATUS", "PLEASE ENTER VALID DETAILS")
-        else:
-            reg = mysql.connector.connect(user='sql6423129', password=' rutppuWASF', host='sql6.freesqldatabase.com',
-',
-                                          database='sql6423129',
-                                          auth_plugin='mysql_native_password')
-
-            mycursor = reg.cursor()
-            sql = "INSERT INTO Login (user, password) VALUE (%s, %s)"
-            val = (self.username_entry.get(), self.password_entry.get())
-            mycursor.execute(sql, val)
-            messagebox.showinfo("PERMISSION", "YOU HAVE SUCCESSFULLY REGISTERED")
-            self.username_entry.delete(0, END)
-            self.password_entry.delete(0, END)
-            reg.commit()
-
-    def exit(self):
-        self.message_box = messagebox.askquestion('Exit Application', 'Are you sure you want to Logout?')
-        if self.message_box == 'yes':
-            self.root.destroy()
-        else:
-            pass
+        reg_btn = Button(root, text='Login', bg='#0F52BA', fg='white', command=login_ad)
+        reg_btn.place(x=155, y=200, width=80)
 
 
-Y = Database(root)
+
+    def admin_login(self):
+    adminPassword = entUserPass.get()
+
+    mydb = mysql.connector.connect(
+                                    host="127.0.0.1",
+                                    user="lifechoices",
+                                    password="@Lifechoices1234",
+                                    database="LifechoicesOnline")
+    mycursor = mydb.cursor()
+    sql = 'SELECT * FROM Registration =%s'
+    mycursor.execute(sql, [adminPassword])
+    res = mycursor.fetchall()
+    if res:
+        for i in res:
+            logged()
+            break
+    else:
+        failed()
+
+
+def logged():
+    messagebox.showinfo('Correct', 'you have successfully logged into admin')
+    pass
+
+
+
+def failed():
+    messagebox.showinfo('Failed', 'Please enter correct admin password')
+    entUserPass.focus()
+
+
+
+
+
+
+
+
+
 root.mainloop()
